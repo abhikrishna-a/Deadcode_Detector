1352ef4ff4717951117071b7a1f4503a2af00685
abhikrishna <abhikrishna616@gmail.com>
2026-07-04 12:58:20 +0530

    fix: install eslint deps properly, relax react-hooks strict rules to warnings

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -19,6 +19,11 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/refs': 'warn',
+      'react-hooks/set-state-in-effect': 'warn',
+      'react-hooks/purity': 'warn',
+      'react-hooks/immutability': 'warn',
+
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
