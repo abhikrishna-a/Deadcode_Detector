b6082aa448b4083ce99abd5effcb6f84d3e0cb5d
abhikrishna <abhikrishna616@gmail.com>
2026-07-12 16:55:58 +0530

    chore: remove temp scripts

--- scripts/check_jobs.py
+++
@@ -1,5 +0,0 @@
-import json, sys
-data = json.load(sys.stdin)
-for j in data['jobs']:
-    c = str(j.get('conclusion') or 'running')
-    print(f"{j['name']:35s} {c}")
--- scripts/check_status.py
+++
@@ -1,5 +0,0 @@
-import json, sys
-runs = json.load(sys.stdin)['workflow_runs']
-for r in runs[:5]:
-    c = str(r.get('conclusion') or 'running')
-    print(f"{r['name']:10s} {r['status']:12s} {c:12s} {r['display_title'][:45]}")
